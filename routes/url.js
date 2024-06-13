a32645a91178ff5352ba4c71d2104d14c9646a6d
adhidhyaRP <153973098+adhidhyaRP@users.noreply.github.com>
2024-06-13 16:43:35 +0530

    Update url.js

--- routes/url.js
+++ routes/url.js
@@ -12,7 +12,7 @@ router.post('/getshortURL', async (req, res) => {
         if (urlObj) return res.send(urlObj.shortURL);
 
         const shortcode = nanoid(7);
-        const shortURL = `http://localhost:3000/${shortcode}`;
+        const shortURL = `https://loginwithurlshortner-3.onrender.com//${shortcode}`;
         urlObj = new URL({ URL: longURL, shortURL, shortcode });
         await urlObj.save();
         res.send(shortURL);
